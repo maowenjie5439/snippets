5858d48f7d28d3ad7ac7640f049b5fff14e71d8f
maowenjie <maowj4@yusys.com.cn>
2025-02-25 16:08:00 +0800

    初始提交

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,15 @@
+import Versions from './components/Versions'
+import electronLogo from './assets/electron.svg'
+
+function App(): JSX.Element {
+
+  return (
+    <>
+      <div className="bg-red-500">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
